bfd194a35cad6eee0a301b1f04586a8125dc7225
ZARIFMAC <uzbzarif@gmail.com>
2022-12-29 11:47:15 +0500

    max

---
+++ task_max.py
@@ -0,0 +1,32 @@
+def find_max(a,b,c):
+    """
+    Find the maximum number.
+    Args:
+
+        a: int
+        b: int
+        c: int
+    return:
+        int
+    """
+    return 0
+
+# Example:
+# find_max(1,2,3) -> 3
+# find_max(-1,12,3) -> 12
+
+def find_max_idx(a,b,c):
+    """
+    Find the index of the maximum number.
+    Args:
+        a: int
+        b: int
+        c: int
+    return:
+        int
+    """
+    return 0
+
+# Example:
+# find_max_idx(10,2,13) -> 3
+# find_max_idx(-1,12,3) -> 2
